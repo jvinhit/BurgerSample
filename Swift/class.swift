589b5650197dbe37ccf2ecb6253ccae5c542852b
jvinhit <jvinhit@gmail.com>
2018-04-08 18:35:07 +0700

    Update

--- Swift/class.swift
+++ Swift/class.swift
@@ -4,6 +4,7 @@ protocol SoundBehavior {
 }
 class Person: SoundBehavior {
     var name:String
+    var middleName:String? // Optional prop
     var age:Int
     var birthDay: String
     init(name: String, age: Int, birthDay: String){
@@ -23,8 +24,22 @@ class Person: SoundBehavior {
     }
     // computed prop
     var showInfo: String {
-        return "My name is " 
+        if let middleName = middleName { // if middleName != nil 
+            return "My FullName : " + middleName + " " + name
+        }
+        return "My FullName : " + name
     }
+    var doubleAge: Int{
+        get {
+            return age * 2;
+        }
+        set {
+            age = newValue;
+        }
+    }
+
+    // Lazy Props: 
+
 }
 
 var p:Person = Person(name: "Vinh", age: 20, birthDay: "20/11")
@@ -32,6 +47,25 @@ var p:Person = Person(name: "Vinh", age: 20, birthDay: "20/11")
 
 var p2:Person = Person()
 
-
+// Protocol
 p.SayHello()
-p2.SayMess(mess: "Vinh dep Trai")
+p2.SayMess(mess: "Vinh dep Trai") 
+print("p2.showInfo : \(p2.showInfo)") 
+// computed Prop
+print("p.showInfo : \(p.showInfo)") 
+print("p.doubleAge : \(p.doubleAge)")
+
+print("Set p.age =  \(p.age)")
+print("get p.doubleAge:  \(p.doubleAge)")
+
+class House {
+    var Rect: Float
+    init(Rect: Float){
+        self.Rect = Rect
+    }
+    // Lazy Props
+    lazy var owner:Person = Person(name: "Vinh", age: 20, birthDay: "20/11")
+}
+
+var house = House(Rect: 300.1)
+print("\(house.owner)")
